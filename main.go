7a28bc74ca0ca172a40917c6b99c4b28e3f7d5d8
Dylan Ramos <dylanr98@gmail.com>
2024-12-01 01:04:59 -0500

    Began form building with Huh?

--- main.go
+++ main.go
@@ -1,17 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"github.com/charmbracelet/huh"
 	"os"
-	"strings"
 	"time"
-
-	"github.com/dustin/go-humanize"
 )
 
 type Task struct {
-	Id          int
 	Title       string
 	Description string
 	Tag         string
@@ -20,54 +16,34 @@ type Task struct {
 }
 
 func main() {
-	fmt.Println("Let's build some tasks!")
-	var tasks []Task
-	scanner := bufio.NewScanner(os.Stdin)
+	var task Task
+	//var taskList []Task
 
-	for {
-		// Prompt user for input
-		fmt.Println("\nEnter task details:")
+	form := huh.NewForm(
+		huh.NewGroup(huh.NewNote().
+			Title("ToDoCLI").
+			Description("Welcome to _ToDoCLI!_.\n\n").
+			Next(true).
+			NextLabel("Proceed"),
+		),
 
-		// Get Title
-		fmt.Print("Title: ")
-		scanner.Scan()
-		title := scanner.Text()
+		// Create a Task
+		huh.NewGroup(huh.NewInput().
+			Title("Task name:").
+			Prompt("* ").
+			Validate(func(t string) error {
+				if t == "" {
+					return fmt.Errorf("please enter a task name")
+				}
+				return nil
+			}).
+			Value(&task.Title),
+		),
+	)
+	err := form.Run()
 
-		// Get Description
-		fmt.Print("Description: ")
-		scanner.Scan()
-		description := scanner.Text()
-
-		// Get Tag
-		fmt.Print("Tag: ")
-		scanner.Scan()
-		tag := scanner.Text()
-
-		// Add Task to List
-		task := Task{
-			Id:          len(tasks) + 1,
-			Title:       title,
-			Description: description,
-			Tag:         tag,
-			Status:      false,
-			Created:     time.Now(),
-		}
-		tasks = append(tasks, task)
-
-		// Ask if user wants to create another task
-		fmt.Print("\nDo you want to create another task? (y/n): ")
-		scanner.Scan()
-		response := strings.ToLower(scanner.Text())
-		if response != "y" {
-			break
-		}
+	if err != nil {
+		fmt.Println("Uh oh:", err)
+		os.Exit(1)
 	}
-
-	// Display all tasks
-	fmt.Println("\nAll Created Tasks:")
-	for _, task := range tasks {
-		fmt.Printf("\nTask #%d\nTitle: %s\nDescription: %s\nTag: %s\nCreated: %s\n",
-			task.Id, task.Title, task.Description, task.Tag, humanize.Time(task.Created))
-	}
-
 }
